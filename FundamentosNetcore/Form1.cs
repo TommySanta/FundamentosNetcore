7dc77293cee408f8a078108bc2c766569edf5018
TommySanta <tomas.santamaria.benito@gmail.com>
2024-12-10 13:09:42 +0100

    Commit uno

--- FundamentosNetcore/Form1.cs
+++ FundamentosNetcore/Form1.cs
@@ -12,7 +12,7 @@ namespace FundamentosNetcore
         private void Button1_Click(object sender, EventArgs e)
         {
             this.nombre.Location = new Point(20,10);
-            this.nombre.Text = "Soy un string de Prueba 2";
+            this.nombre.Text = "Soy un string de Prueba";
             this.nombre.Width = 220;
             this.nombre.TextAlign = HorizontalAlignment.Right;
             this.button1.BackColor = Color.Azure;
